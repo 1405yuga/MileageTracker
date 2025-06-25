eeced02ed08f2599dc24740d11757948f7d6221b
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-06-25 11:56:32 +0530

    created annotations for preview

---
+++ app/src/main/java/com/example/mileagetracker/utils/annotations/HorizontalScreenPreview.kt
@@ -0,0 +1,10 @@
+package com.example.mileagetracker.utils.annotations
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    showBackground = true,
+    widthDp = 640,
+    heightDp = 360
+)
+annotation class HorizontalScreenPreview
