717bd4e1ce147c8418694e2e5f1308699b170821
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-06-25 11:58:22 +0530

    created main screens

---
+++ app/src/main/java/com/example/mileagetracker/ui/screens/tracker/TrackerScreen.kt
@@ -0,0 +1,10 @@
+package com.example.mileagetracker.ui.screens.tracker
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TrackerScreen(modifier: Modifier = Modifier) {
+    Text("Tracker")
+}
